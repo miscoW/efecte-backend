d007098661355e5ae059af9230da5b7ee7f2e345
mieszkowaw <wawrzyniakmieszko@gmail.com>
2022-09-26 19:00:20 +0200

    initial version

---
+++ src/test/java/pl/mwawrzyn/efectebackend/EfecteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mwawrzyn.efectebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfecteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
